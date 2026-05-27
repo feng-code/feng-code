121ded471e00455788f4c890a1f787572798ff67
feng-code <62631008+feng-code@users.noreply.github.com>
2026-05-28 00:13:58 +0800

    feat: enable course paths and engineering review cards

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import "./design-system.css";
 import "./theme-extensions.css";
 import "./experience-enhancements.css";
 import "./search-enhancements.css";
+import "./course-path-enhancements.css";
+import "./engineering-review.css";
 import "./theme-extensions.js";
 import "./experience-enhancements.js";
 import "./search-enhancements.js";
+import "./course-path-enhancements.js";
+import "./engineering-review.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
