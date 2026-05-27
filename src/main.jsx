dd0cc75f114e5c21d05fdde27cefd6f272cbf347
feng-code <62631008+feng-code@users.noreply.github.com>
2026-05-27 22:58:37 +0800

    feat: load extended theme styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./design-system.css";
+import "./theme-extensions.css";
 import "./theme-extensions.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
